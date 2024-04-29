0e8da61ce262bcfc200ecba7e0dc95d2c3f4d1e8
saad-official <tommmyy890@gmail.com>
2024-04-29 08:12:20 +0500

    add database connection

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import  express, {Response, Request}  from "express";
 import cors from "cors";
 import "dotenv/config";
-
+import mongoose from "mongoose"
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Database Connected Successfully ss"))
+
+  .catch((err) => console.log(err));
+
 
 
 app.get("/test", (req:Request, res:Response) => {
