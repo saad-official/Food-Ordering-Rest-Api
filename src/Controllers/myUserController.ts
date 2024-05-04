c8acfc208d851d70aa92c61c4d24293182836f12
SadiPro07 <saad.khan@zortik.com>
2024-05-04 13:36:48 +0500

    complete user create api

--- src/Controllers/myUserController.ts
+++ src/Controllers/myUserController.ts
@@ -1,9 +1,29 @@
 import { Request, Response } from "express";
+import { User } from "../models/user";
 
 const createCurrentUser = async (req: Request, res: Response) => {
   // 1. Check if Current User Exist
   // 2. Create the User if it doesn't exist
   // 3. return the user object to the calling client
+
+  try {
+    const { auth0Id } = req.body;
+    const existingUser = await User.findOne({ auth0Id });
+
+    if (existingUser) {
+      return res.status(200).send();
+    }
+
+    console.log("come");
+
+    const newUser = new User(req.body);
+    await newUser.save();
+
+    return res.status(201).json(newUser.toObject());
+  } catch (error) {
+    console.log("err");
+    return res.status(400).send();
+  }
 };
 
 export default {
